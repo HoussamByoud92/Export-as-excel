e3ce4282813a1ea349551aa34d1c274c4a95845f
Houssam Byoud <byoudhoussam@gmail.com>
2025-01-02 20:46:24 +0100

    Added data importation from XLSX file.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace WindowsFormsApp1
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -87,11 +88,22 @@ namespace WindowsFormsApp1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(603, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Importer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tabControl1);
@@ -111,6 +123,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -120,5 +120,93 @@ namespace WindowsFormsApp1
                 }
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "server=localhost;database=HotelManagementSystem;uid=root;pwd=;";
+                string query = "SELECT * FROM rooms";
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                  
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    openFileDialog.Filter = "Excel Files|*.xlsx";
+                    openFileDialog.Title = "Select an Excel File";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string excelFilePath = openFileDialog.FileName;
+
+                       
+                        DataTable dataTable2 = new DataTable();
+                        using (XLWorkbook workbook = new XLWorkbook(excelFilePath))
+                        {
+                            IXLWorksheet worksheet = workbook.Worksheet(1); 
+                            bool firstRow = true;
+
+                            foreach (IXLRow row in worksheet.RowsUsed())
+                            {
+                                if (firstRow)
+                                {
+                                    foreach (IXLCell cell in row.Cells())
+                                    {
+                                        dataTable2.Columns.Add(cell.Value.ToString());
+                                    }
+                                    firstRow = false;
+                                }
+                                else
+                                {
+                                    dataTable2.Rows.Add(row.Cells().Select(cell => cell.Value.ToString()).ToArray());
+                                }
+                            }
+                        }
+
+                        MessageBox.Show($"DataTable has {dataTable2.Rows.Count} rows and {dataTable2.Columns.Count} columns.");
+
+                        
+                        using (MySqlConnection connection = new MySqlConnection(connectionString))
+                        {
+                            connection.Open();
+
+                            foreach (DataRow row in dataTable2.Rows)
+                            {
+                                string description = row["Description"] != null ? row["Description"].ToString() : null;
+
+                                string insertQuery = @"
+                    INSERT INTO rooms (RoomID, RoomNumber, RoomTypeID, Status, PricePerNight, Description)
+                    VALUES (@RoomID, @RoomNumber, @RoomTypeID, @Status, @PricePerNight, @Description)";
+
+                                using (MySqlCommand command = new MySqlCommand(insertQuery, connection))
+                                {
+                                    command.Parameters.AddWithValue("@RoomID", Convert.ToInt32(row["RoomID"]));
+                                    command.Parameters.AddWithValue("@RoomNumber", row["RoomNumber"].ToString());
+                                    command.Parameters.AddWithValue("@RoomTypeID", Convert.ToInt32(row["RoomTypeID"]));
+                                    command.Parameters.AddWithValue("@Status", row["Status"].ToString());
+                                    command.Parameters.AddWithValue("@PricePerNight", Convert.ToDecimal(row["PricePerNight"]));
+                                    command.Parameters.AddWithValue("@Description", description);
+
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+
+                            MessageBox.Show("Data imported successfully into the 'rooms' table.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
